85c64985c6997f6308c432ea0430d718c14c3386
krosswelcauro <krosswelcauro@gmail.com>
2025-11-02 22:39:50 -0400

    feat (style): agregando la seccion galeria y creando un modal para visualizar la imagen

---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+// Manipular el DOM para crear una galería de imágenes
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria()
+});
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes'); 
+
+    for( let i = 1; i <= 16; i++ ) {
+        const imagen = document.createElement('IMG');
+        imagen.src = `src/img/gallery/full/${ i }.jpg`;
+        imagen.alt = `Imagen galeria`;
+
+        // Event Handler - Proceso para detectar el click en la imagen
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+    }
+}
+
+function mostrarImagen(i) {
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = `Imagen galeria`;
+
+  // Generar modal
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
+
+  modal.appendChild(imagen);
+
+  // Agregar al HTML
+  const body = document.querySelector("body");
+  body.classList.add('overflow-hidden');
+  body.appendChild(modal);
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out');
+
+    setTimeout(() => {
+        modal?.remove(); // El signo de interrogación es para validar si el modal existe
+        const body = document.querySelector("body");
+        body.classList.remove('overflow-hidden');
+    }, 500)
+
+}
