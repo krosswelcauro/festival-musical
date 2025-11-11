3de57aa5084b432d01f02e409a9f14cf0d9b6102
krosswelcauro <krosswelcauro@gmail.com>
2025-11-11 03:07:06 -0400

    feat (style): agregando funcion de cerrar modal en la galeria y ajuste de carga de imagenes y header

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,33 @@
-import { src, dest, watch, series } from 'gulp';
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
+import { src, dest, watch, series } from "gulp";
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
 
 const sass = gulpSass(dartSass);
 
-export function js (done) {
+export function js(done) {
+  src("src/js/app.js").pipe(dest("build/js"));
 
-    src('src/js/app.js')
-        .pipe(dest('build/js'));
-
-    done()
+  done();
 }
-    
 
-export function css (done) {
-    src('src/scss/app.scss', { sourcemaps: true })
-        .pipe(sass().on('error', sass.logError))
-        .pipe(dest('build/css'), { sourcemaps: '.' });
+export function img(done) {
+  src("src/img/**/*").pipe(dest("build/img"));
 
-    done();
+  done();
+}
+
+export function css(done) {
+  src("src/scss/app.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("build/css"), { sourcemaps: "." });
+
+  done();
 }
 
 export function dev() {
-    watch('src/scss/**/*.scss', css);
-    watch('src/js/**/*.js', js);
+  watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", js);
+  watch("src/img/**/*.jpg", img);
 }
 
-export default series( js, css, dev );
+export default series(js, img, css, dev);
